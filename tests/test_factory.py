804a115a1e04b30ec6acc427d4837b5dfdaaaf5f
theodericleon <46503929+theodericleon@users.noreply.github.com>
2019-01-22 20:11:04 -0600

    added factory tests

---
+++ tests/test_factory.py
@@ -0,0 +1,9 @@
+from app import create_app
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING':True}).testing
+
+def test_maintenance(client):
+    response = client.get('maintenance')
+    assert response.data == b'Under maintenance, come back soon!'
