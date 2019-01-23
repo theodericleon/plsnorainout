10927818f8a443affa196a864d4748ad4145e5d0
theodericleon <46503929+theodericleon@users.noreply.github.com>
2019-01-22 21:48:07 -0600

    corrected typos in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 from app import create_app
 from app.db import get_db, init_db
 
-with open(os.path.join(os.dirname(__file__), 'data.dql'), 'rb') as f:
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
     _data_sql = f.read().decode('utf8')
 
 @pytest.fixture
