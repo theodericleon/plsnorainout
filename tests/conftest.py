fc6741d2f4eaf9fde03102f95c69a7c1357ebd88
theodericleon <46503929+theodericleon@users.noreply.github.com>
2019-01-25 20:35:52 -0600

    a few different attempts at getting flask-testing to work with selenium

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
 import os
 import tempfile
+import time
+import multiprocessing
+import socket
+import signal
+import logging
 
 import pytest
+from flask import Flask
+from flask_testing import LiveServerTestCase
 from app import create_app
 from app.db import get_db, init_db
 
@@ -51,19 +58,26 @@ class AuthActions(object):
 def auth(client):
         return AuthActions(client)
 
-@pytest.mark.usefixtures('live_server')
-class TestServer(object):
-    def __init__(self, live_server):
-        self._live_server = live_server
+class LiveServer(LiveServerTestCase):
+    def create_app(self):
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        app.config['LIVESERVER_TIMEOUT'] = 15
+
+        with app.app_context():
+            init_db()
+            get_db().executescript(_data_sql)
+
+        return app
 
 @pytest.fixture
-def test_server(live_server):
-    return TestServer(live_server)
+def live_server():
+    return LiveServer()
 
 @pytest.fixture
-def sel_driver(client, test_server):
+def sel_driver(client, live_server):
     from selenium import webdriver
     sel_driver = webdriver.Chrome('C:/Users/Derrick Milner/Documents/chromedriver.exe')
-
+    sel_driver.get('http://localhost/:5000')
     yield sel_driver
     sel_driver.quit()
--- tests/test_base_ui.py
+++ tests/test_base_ui.py
@@ -1,9 +1,44 @@
 import pytest
+import urllib3
 import time
-from flask import url_for
-
+import os
+import tempfile
+from flask import Flask
+from flask_testing import LiveServerTestCase
+from app import create_app
+from app.db import get_db, init_db
+"""
 def test_nav_to_register(client, app, sel_driver):
-    sel_driver.get('http://localhost/')
-    sel_driver.find_element_by_id("register_link").click()
-    time.sleep(1)
-    assert sel_driver.current_url == 'http://localhost/auth/register'
+    sel_driver.get('http://localhost/:5000')
+    time.sleep(5)
+"""
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+    _data_sql = f.read().decode('utf8')
+
+class LiveServer(LiveServerTestCase):
+    def create_app(self):
+        db_fd, db_path = tempfile.mkstemp()
+
+
+        app = create_app({
+            'TESTING': True,
+            'DATABASE':db_path,
+            'LIVESERVER_TIMEOUT':15
+        })
+
+        return app
+
+    def setUp(self):
+        with app.app_context():
+            init_db()
+            get_db().executescript(_data_sql)
+
+        self.driver = webdriver.Chrome('C:/Users/Derrick Milner/Documents/chromedriver.exe')
+        self.driver.get('http://localhost/:5000')
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_server_is_up_and_running(self):
+        response = urllib2.urlopen(self.get_server_url())
+        assert response.code == 200
