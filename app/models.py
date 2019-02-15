513527914603bb87a9d05233aeba0fd469c9d04c
theodericleon <theoderic@disroot.org>
2019-02-15 09:27:57 -0600

    added masks to the models module

--- app/models.py
+++ app/models.py
@@ -8,3 +8,11 @@ class User(db.Model):
     username = db.Column(db.String(60), unique = True, nullable = False)
     password_hash = db.Column(db.String(128), nullable = False)
     zip_code = db.Column(db.Integer, nullable = False)
+
+class Mask(db.Model):
+    __tablename__ = 'masks'
+
+    id = db.Column(db.Integer, primary_key = True)
+    mask_name = db.Column(db.String(60), nullable = False)
+    manufacturer = db.Column(db.String(60), nullable = False)
+    type = db.Column(db.String(60), nullable = False)
