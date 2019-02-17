c7d1478bd4c38671edc15ba25931b426ae2d2e31
theodericleon <theoderic@disroot.org>
2019-02-17 03:55:01 -0600

    added a test for the populate database function and the command

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,5 +1,6 @@
 import pytest
-from app.database import db
+from app.database import db, init_database
+from app.database_utils import populate_database
 from app.models import Mask
 
 def test_init_database_command(runner, monkeypatch):
@@ -13,3 +14,21 @@ def test_init_database_command(runner, monkeypatch):
     result = runner.invoke(args=['init-database'])
     assert 'Initialized' in result.output
     assert Recorder.called
+
+def test_populate_database_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+
+    def fake_pop_db():
+        Recorder.called = True
+
+    monkeypatch.setattr('app.database_utils.populate_database', fake_pop_db)
+    result = runner.invoke(args=['populate-database'])
+    assert 'Populated' in result.output
+    assert Recorder.called
+
+def test_populate_database_function(app):
+    with app.app_context():
+        init_database()
+        populate_database()
+        assert Mask.query.count() == 2
